754768c834139d3a18cff14cff5f45b82ce35ea7
sandrobakra <sandrobakradze0@gmail.com>
2024-01-04 19:54:42 +0200

    add migratios main commit

---
+++ Desktop/E-commerce/E-Commerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Desktop/E-commerce/E-Commerce/Models/category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace E_Commerce.Models
+{
+	public class EmptyClass
+	{
+        [Key]
+		public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
+
